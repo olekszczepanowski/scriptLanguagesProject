aa971b3c6afc135c51679f81ded8cc3120a39374
olekszczepanowski <olekszczepanowski04@wp.pl>
2024-06-11 19:50:10 +0200

    init: create django project

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-
-from . import views
+from .views import TasksList, TaskDetails, CreateTask, UpdateTask, DeleteTask, CustomLoginView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path("", TasksList.as_view(), name="tasks"),
+    path("task/<int:pk>", TaskDetails.as_view(), name="task"),
+    path("task-create/", CreateTask.as_view(), name="task-create"),
+    path("task-update/<int:pk>", UpdateTask.as_view(), name="task-update"),
+    path("task-delete/<int:pk>", DeleteTask.as_view(), name="task-delete"),
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -1,7 +1,46 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
+from todolist.models import Task
 
 
-def index(request):
-    return HttpResponse("To do list")
+class TasksList(ListView):
+    model = Task
+    context_object_name = 'tasks'
+    template_name = 'todolist/task_list.html'
+
+
+class TaskDetails(DetailView):
+    model = Task
+    context_object_name = 'task_details'
+    template_name = 'todolist/task_details.html'
+
+
+class CreateTask(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+
+class UpdateTask(UpdateView):
+    model = Task
+    success_url = reverse_lazy('tasks')
+    fields = '__all__'
+
+
+class DeleteTask(DeleteView):
+    model = Task
+    success_url = reverse_lazy('tasks')
+    context_object_name = 'task'
+
+class CustomLoginView(LoginView):
+    template_name = "todolist/login.html"
+    fields = '__all__'
+    redirect_authenticated_user = True
+    def get_success_url(self):
+        return reverse_lazy('tasks')
